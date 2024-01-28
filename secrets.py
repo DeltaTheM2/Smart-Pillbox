b3026e1c9bff1f91944601fdb165c5191787d3d5
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2024-01-27 16:59:20 -0800

    changes

--- secrets.py
+++ secrets.py
@@ -5,11 +5,3 @@ secrets = {
     "ssid": "CodingMinds",
     "password": "codingmindsgo!",
 }
-
-# Write your code here :-)
-# This file is where you keep secret settings, passwords, and tokens!
-# If you put them in the code you risk committing that info or sharing it
-secrets = {
-    "ssid": "CodingMinds",
-    "password": "codingmindsgo!",
-}
