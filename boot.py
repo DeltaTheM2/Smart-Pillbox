57beb7989eaa72f8da61881539d8b562ed539c1a
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2024-05-04 16:52:38 -0700

    Update boot.py

--- boot.py
+++ boot.py
@@ -5,37 +5,41 @@ import adafruit_requests
 import storage
 import microcontroller
 import binascii
-
+import ssl
+storage.remount("/",readonly=False,disable_concurrent_write_protection=True)
 def get_or_create_device_id(filename="device_id.txt"):
-    # Check if the device_id.txt file exists
     if filename in os.listdir("/"):
         with open(filename, "r") as file:
             device_id = file.read().strip()
-            print("Existing Device ID:", device_id)
+            print("Existing Device ID: ", device_id)
             return device_id
     else:
-        # Generate a new device ID. This example uses the CPU's unique ID.
-        device_id = binascii.hexlify(microcontroller.cpu.uid).decode("utf-8")
+        device_id = binascii.hexify(microcontroller.cpu.uid).decode("uft-8")
         with open(filename, "w") as file:
             file.write(device_id)
         print("New Device ID:", device_id)
         return device_id
 
-# Attempt to connect to Wi-Fi
+def get_qrcode(device_id=get_or_create_device_id()):
+    response = requests.get(f"https://smart-pillbox-server.onrender.com/register_device/{device_id}")
+    if response.status_code == 200:
+        with open("qrcode.bmp", 'wb') as file:
+            file.write(response.content)
+        print("qr saved")
+    else:
+        print("failed to download the bmp from server")
+
 try:
-    print("Connecting to Wi-Fi...")
-    wifi.radio.connect(ssid="YOUR_SSID", password="YOUR_PASSWORD")
-    print("Connected to Wi-Fi")
-except Exception as e:
-    print("Failed to connect to Wi-Fi:", e)
-    # Consider handling reconnection or alerting the user here.
-
-# Initialize the requests library for future HTTP requests
+    wifi.radio.connect(
+        os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD")
+    )
+    print("Connected to %s!" % os.getenv("CIRCUITPY_WIFI_SSID"))
+except Exception as e:  # pylint: disable=broad-except
+    print(
+        "Failed to connect to WiFi. Error:", e, "\nBoard will hard reset in 30 seconds."
+    )
 pool = socketpool.SocketPool(wifi.radio)
-requests = adafruit_requests.Session(pool, None)
+requests = adafruit_requests.Session(pool, ssl.create_default_context())
+print("getting qrcode")
+get_qrcode()
 
-# Generate or read the existing device ID
-device_id = get_or_create_device_id()
-
-# Note: Further logic to check device registration could go here or in code.py,
-# depending on when you want to perform the check and how it impacts your application flow.
